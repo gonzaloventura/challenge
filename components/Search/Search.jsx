1fe6ef83d26f899428c9b167ed42eb1c4de4630f
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-09-29 23:37:08 -0300

    Creacion del componente

---
+++ components/Search/Search.jsx
@@ -0,0 +1,7 @@
+export default function Search() {
+    return(
+        <div className="w-full rounded bg-white max-w-lg" >
+            <input type="text" className="w-full" />
+        </div>
+    )
+}
