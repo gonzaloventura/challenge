1b297cc6dbbbb937b75536f649d56979113a3970
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-09-30 01:36:56 -0300

    mejoras visuales

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -1,7 +1,9 @@
+import './search.css'
+
 export default function Search() {
     return(
-        <div className="w-full rounded bg-white max-w-lg" >
-            <input type="text" className="w-full" />
+        <div className="" >
+            <input type="text" className="search" placeholder='Buscar máquina' />
         </div>
     )
 }
