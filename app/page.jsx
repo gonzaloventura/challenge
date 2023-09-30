565d398d11b6198ba7fafa0c937374c88a4cd014
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-09-29 23:36:14 -0300

    default

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import InfoContainer from "@/components/InfoContainer/InfoContainer"
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        <InfoContainer />
+      </div>
+    </main>
+  )
+}
