5951360f573a2a49b1dcfcb6cabb106d07da35c0
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-09-30 01:39:47 -0300

    prueba para chequear visualizacion

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,13 @@ import InfoContainer from "@/components/InfoContainer/InfoContainer"
 
 export default function Home() {
   return (
-    <main className="">
-      <div>
+    <main className="p-10">
+      <div className="grid grid-cols-3 gap-4">
+        <InfoContainer />
+        <InfoContainer />
+        <InfoContainer />
+        <InfoContainer />
+        <InfoContainer />
         <InfoContainer />
       </div>
     </main>
