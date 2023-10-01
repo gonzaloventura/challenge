d63a3ed9173703bb46ac71623e086f7bcf7340e1
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-10-01 01:07:14 -0300

    fetch de la data

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,29 @@
 import InfoContainer from "@/components/InfoContainer/InfoContainer"
 
-export default function Home() {
+const getData = () => {
+  return fetch(`https://wrk.acronex.com/api/challenge/machines/`, { cache: 'no-store' })
+    .then(res => res.json())
+    .catch(err => console.log(err))
+}
+
+export default async function Home() {
+  const data = await getData()
   return (
     <main className="p-10">
       <div className="grid grid-cols-3 gap-4">
+        {data.map((data) => {
+          return (
+            <InfoContainer data={data} />
+          )
+        })}
+        {/*         
         <InfoContainer />
         <InfoContainer />
         <InfoContainer />
         <InfoContainer />
         <InfoContainer />
-        <InfoContainer />
+        <InfoContainer /> 
+        */}
       </div>
     </main>
   )
